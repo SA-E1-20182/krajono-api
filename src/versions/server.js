adb2e8982ec93c207fc9b97a34ec72577faf8f03
fffeelipe <lfepiar@unal.edu.co>
2018-10-19 12:17:40 -0500

    version not working

---
+++ src/versions/server.js
@@ -0,0 +1,3 @@
+export const url = process.env.KRAJONO_URL
+export const port = process.env.VERSION_PORT
+export const entryPoint = process.env.VERSION_ENTRY
---
+++ src/versions/typeDefs.js
@@ -0,0 +1,28 @@
+export const projectsTypeDef = `
+type Project {
+    id: Int!
+    name: String!
+    genre: String!
+    description: String!
+    created_at: String!
+    total_pages: Int!
+    author_id: String!
+    current_version_id: String!
+}
+
+input ProjectInput {
+    name: String!
+    description: String!
+    genre: String!
+}`;
+
+export const projectsQueries = `
+    allProjects: [Project]!
+    projectByCode(code: Int!): Project!
+`;
+
+export const projectsMutations = `
+    createProject(project: ProjectInput!): Project!
+    deleteProject(code: Int!): Int
+    updateProject(code: Int!, project: ProjectInput!): Project!
+`;
