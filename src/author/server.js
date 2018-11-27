5617d4d8639e3ed7287b16628d29fee9c5bf0284
unknown <rcbuelvasv@gmail.com>
2018-11-26 20:54:22 -0500

    También funciona (?

--- src/author/server.js
+++
@@ -1,3 +0,0 @@
-export const url = process.env.KRAJONO_URL
-export const port = process.env.AUTHOR_PORT
-export const entryPoint = process.env.AUTHOR_ENTRY
--- src/authors/server.js
+++ src/authors/server.js
@@ -1,5 +1,3 @@
 export const url = process.env.KRAJONO_URL
-export const port = process.env.AUTHOR_PORT
-export const entryPointCreate = process.env.AUTHOR_CREATE
-export const entryPointSearch = process.env.AUTHOR_SEARCH
-export const entryPointAuthor = process.env.AUTHOR_AUTHOR
+export const port = process.env.AUTHORS_PORT
+export const entryPoint = process.env.AUTHORS_ENTRY
