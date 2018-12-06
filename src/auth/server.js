20899684d710f7901d20fd64f7419ee6846eea52
fffeelipe <lfepiar@unal.edu.co>
2018-12-06 08:27:05 -0500

    unified url in auth

--- src/auth/server.js
+++ src/auth/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.AUTH_URL || ' 192.168.99.101';
+export const url = process.env.KRAJONO_URL || ' 192.168.99.101';
 export const port = process.env.AUTH_PORT || '3009';
 export const entryPoint = process.env.AUTH_ENTRY || 'ldap';
