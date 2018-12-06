9b8d4260d2397d396f6db43d878c1fb5398b3489
fffeelipe <lfepiar@unal.edu.co>
2018-12-06 07:01:49 -0500

    Deleted optional data en user

--- src/users/server.js
+++ src/users/server.js
@@ -1,3 +1,3 @@
-export const url = process.env.USERS_URL || '192.168.99.101'
-export const port = process.env.USERS_PORT || '3009'
-export const entryPoint = process.env.USERS_ENTRY || ''
+export const url = process.env.USERS_URL
+export const port = process.env.USERS_PORT
+export const entryPoint = process.env.USERS_ENTRY
